2ea85be29542f4d8c3d990bd40fc8fa3bd628635
“lukassperandio” <emanuelsperandio@gmail.com>
2026-05-04 14:03:59 +0100

    Project created

---
+++ NewToDoList/src/main/java/com/sperandio/lukas/NewToDoList/NewToDoListApplication.java
@@ -0,0 +1,13 @@
+package com.sperandio.lukas.NewToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewToDoListApplication.class, args);
+	}
+
+}
