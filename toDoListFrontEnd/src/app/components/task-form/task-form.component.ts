c19922840615787a59cea9f9061d0e0adbb6a0bb
“lukassperandio” <emanuelsperandio@gmail.com>
2026-05-09 18:41:18 +0100

    feat: project structure created

---
+++ toDoListFrontEnd/src/app/components/task-form/task-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form',
+  imports: [],
+  templateUrl: './task-form.component.html',
+  styleUrl: './task-form.component.css',
+})
+export class TaskFormComponent {}
